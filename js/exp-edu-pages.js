fb517723961f1e40f9cec6df5177387872fe32d0
George Paresishvili <68405049+macron125@users.noreply.github.com>
2021-07-19 01:06:49 +0400

    New Feature
    Added a new horizontal scrolling feature under "Experience", Section 3.

--- js/exp-edu-pages.js
+++ js/exp-edu-pages.js
@@ -1,30 +1,47 @@
-let text = document.querySelector('.work-text-wrapper');
+let text = document.querySelector(".work-text-wrapper");
+window.addEventListener("DOMContentLoaded", () => {
+  createGoBackBtn();
+});
 
 function createGoBackBtn() {
-  for(let i = 1; i < text.children.length; i++) {
-    let goBackBtn = document.createElement('button');
-    goBackBtn.classList.add('goBack');
+  for (let i = 1; i < text.children.length; i++) {
+    let goBackBtn = document.createElement("button");
+    goBackBtn.classList.add("goBack");
     goBackBtn.innerText = "< Go Back";
-    goBackBtn.setAttribute("onClick", "goBack(text)")
-    text.children[i].appendChild(goBackBtn)
+    goBackBtn.setAttribute("onClick", "goBack(text)");
+    text.children[i].appendChild(goBackBtn);
   }
 }
 
 function goBack(parent) {
-  for(let child of parent.children) {
-    child.style.transform = "translateX(0%)"
+  for (let child of parent.children) {
+    anime({
+      targets: child,
+      translateX: "0%",
+      easing: "easeOutElastic",
+    });
   }
 }
 
-createGoBackBtn();
-
-let jobList = document.querySelector('.job-list ul')
-for(let i = 0; i < jobList.children.length; i++) {
+let jobList = document.querySelector(".job-list ul");
+for (let i = 0; i < jobList.children.length; i++) {
   let currentChild = jobList.children[i];
-  currentChild.addEventListener('click', () => {
-    for(let l of text.children) {
-      l.style.transform = `translateX(-${i + 1 + "00%"})`
-      l.style.cursor = "pointer"
-    }
-  })
+  if (currentChild.classList.value != "empty") {
+    currentChild.addEventListener("click", () => {
+      for (let l of text.children) {
+        animate(l, i);
+      }
+    });
+  }
+}
+
+function animate(target, turn) {
+  anime({
+    targets: target,
+    translateX: {
+      value: `-${turn + 1 + "00%"}`,
+      duration: 1200,
+      easing: "easeOutExpo",
+    },
+  });
 }
