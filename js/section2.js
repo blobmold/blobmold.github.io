c4b2923b04c531381bd0d45599a27e8393e40b37
George Paresishvili <68405049+macron125@users.noreply.github.com>
2021-08-05 22:30:40 +0400

    Update of Section 2
    Now age is changing depending on the current year. Other elements in the Section 2 > Personal Details are now appended via JS

--- js/section2.js
+++ js/section2.js
@@ -1,30 +1,31 @@
-    class PersonalDetails {   //section 2
-      constructor(name, location) {
-        this.name = name;
-        this.age = 0;
-        this.location = location
-      }
+class PersonalDetails {   //section 2
+  constructor(name, location) {
+    this.name = name;
+    this.age = 0;
+    this.location = location
+  }
 
-      setAge(...birthday) {
-        let today = new Date();
-        let birthDate = new Date(...birthday);
-        this.age = today.getFullYear() - birthDate.getFullYear();
-        let month = today.getMonth() - birthDate.getMonth();
-        if(month < 0 || (month === 0 && today.getDate() - birthDate.getDate())) {
-          return this.age--
-        }
-      }
+  setAge(...birthday) {
+    let today = new Date();
+    let birthDate = new Date(...birthday);
+    this.age = today.getFullYear() - birthDate.getFullYear();
+    let month = today.getMonth() - birthDate.getMonth();
+    if(month < 0 || (month === 0 && today.getDate() - birthDate.getDate())) {
+      return this.age--
     }
+  }
+}
 
-    let personalDetails = new PersonalDetails("George Paresishvili", "Tbilisi, Georgia")
-    personalDetails.setAge(1995, 8, 10)
-    console.log(personalDetails)
+let personalDetails = new PersonalDetails("George Paresishvili", "Tbilisi, Georgia")
+personalDetails.setAge(1995, 8, 10)
+console.log(personalDetails)
 
-    let sect2Details = document.querySelector(".sect2-details");
-    for(let entry of Object.entries(personalDetails)) {
-      let newSect2H3El = document.createElement("h3");
-      let newSect2PEl = document.createElement("p")
-      newSect2H3El.innerText = entry[0].toUpperCase();
-      newSect2PEl.innerText = entry[1];
-      sect2Details.append(newSect2H3El, newSect2PEl)
-    }
+let sect2Details = document.querySelector(".sect2-details");
+for(let entry of Object.entries(personalDetails)) {
+  console.log(entry[0].split("")[0].toUpperCase() + entry[0].slice(1))
+  let newSect2H3El = document.createElement("h3");
+  let newSect2PEl = document.createElement("p")
+  newSect2H3El.innerText = entry[0].split("")[0].toUpperCase() + entry[0].slice(1);
+  newSect2PEl.innerText = entry[1];
+  sect2Details.append(newSect2H3El, newSect2PEl)
+}
